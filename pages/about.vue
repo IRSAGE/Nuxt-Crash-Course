de2fc7ea4874194a5b84f40dfd01d33a7a57f3be
IRSAGE <sibo.egide98@gmail.com>
2022-11-11 11:09:44 +0200

    Adding Pages

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, dolore
+      temporibus vero excepturi in fugit eveniet cumque reiciendis qui officiis.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, dolore
+      temporibus vero excepturi in fugit eveniet cumque reiciendis qui officiis.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
